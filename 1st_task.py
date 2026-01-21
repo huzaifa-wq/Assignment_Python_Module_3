1d88c505fb5f68f5bce0e6090260afa43c27d1e5
huzaifa-wq <bhaihuzaifa733@gmail.com>
2026-01-21 22:54:42 +0500

    1st task

---
+++ 1st_task.py
@@ -0,0 +1,7 @@
+zander_length = int(input("enter the length of the zander in centimeters: "))
+size_limit = 42
+standerd_size = size_limit - zander_length
+if zander_length <= size_limit :
+    print(" release the fish in the lack because your fish is",standerd_size,"cm small then standerd size")
+else:
+    print("Nice fish size")
