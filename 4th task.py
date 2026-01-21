255b1e37bb110617f0a4dc2ffaa8b6f9f697e6af
huzaifa-wq <bhaihuzaifa733@gmail.com>
2026-01-22 00:23:03 +0500

    4th task

---
+++ 4th task.py
@@ -0,0 +1,10 @@
+year = int(input("Enter the year: "))
+
+if year % 4 == 0:
+    print("The year is leap year",)
+elif year % 100 != 0:
+    print("The year is not leap year",)
+elif year % 400 == 0:
+    print("The year is leap year",)
+else:
+    print("The year is not leap year")
